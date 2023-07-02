d2867b493263ef4452dc8c7751f96136846c0ed4
Ceren Şahin <cerennssahinn@gmail.com>
2023-07-02 10:28:43 +0300

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+  const projectList = projects.map((project) => {
+    const { id, name, about, technologies } = project;
+    return <div key={id}><ProjectItem key={id} about={about} name={name} technologies={technologies} /></div>
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectList}</div>
     </div>
   );
 }
